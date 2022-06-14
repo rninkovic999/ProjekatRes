76bab90a3f9eb4bf21fc7205ef9c89a00661dba2
Jovan-Draganic <84075897+Jovan-Draganic@users.noreply.github.com>
2022-06-14 17:25:47 +0200

    Add files via upload

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self,id,meterId,address,name,lastname):
+        self.id=id
+        self.meterId=meterId
+        self.name=name
+        self.lastname=lastname
+        self.address=address
+    
