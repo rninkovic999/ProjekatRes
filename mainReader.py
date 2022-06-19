fff35ae0d68f5227590b4a49221d5ba0d188ec54
rninkovic999 <racunaribl7@gmail.com>
2022-06-19 04:18:48 -0700

    Add files via upload
    mala ispravka ispisa

--- mainReader.py
+++ mainReader.py
@@ -2,8 +2,9 @@ from reader import Reader
 if __name__ == "__main__":
     reader=Reader()
     ulaz=-1
-    while(ulaz!=6):
-        print('\n1. Ispis svih user-a\n\
+    while(True):
+        print('\nIzaberite opciju:\n\
+	    1. Ispis svih user-a\n\
             2. Ispis svih merenja\n\
             3. Ispis svih merenja za odredjeni mesec\n\
             4. Ispis svih merenja za korisnika\n\
@@ -17,16 +18,16 @@ if __name__ == "__main__":
         elif ulaz==2:
             reader.ReadAllValues()
         elif ulaz==3:
-            date=input("Unesite mesec i godinu u obliku mm/yyyy (pr:6/2022)")
+            date=input("Unesite mesec i godinu u obliku mm/yyyy (pr:6/2022): ")
             reader.ReadAllValuesByDate(date)
         elif ulaz==4:
             id=int(input("Unesite id user-a:"))
             reader.ReadAllValuesByUserId(id)
         elif ulaz==5:
-            addr=input("Unesite adresu")
+            addr=input("Unesite adresu: ")
             reader.ReadAllValuesByAddr(addr)
         elif ulaz==6:
-            city=input("Unesite grad")
+            city=input("Unesite grad: ")
             reader.ReadAllValuesByCity(city)
         elif ulaz==7:
             exit()
